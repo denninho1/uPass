c395ea406faa21e176c6967632979f97006c3f8c
Dennis <dennis.dennis@gmail.com>
2022-04-17 20:11:09 -0300

    copiar funcionando/refatorei codigo

--- js/generatepass.js
+++ js/generatepass.js
@@ -9,15 +9,17 @@ window.addEventListener('load', () => {
 /* 
 ------- GENERATE PASSWORD -------
 */
-const generateInput = document.querySelector('#input_generate'),
-      passLength = document.querySelector('#input_length'),
-      generateText = document.querySelector('#generate_text'),
-      checkboxUppercase = document.querySelector('#input_uppercase'),
-      checkboxLowercase = document.querySelector('#input_lowercase'),
-      checkboxNumbers = document.querySelector('#input_numbers'),
-      checkboxSymbols = document.querySelector('#input_symbols'),
-      generateBtn = document.querySelector('#generate_btn'),
-      generateBtnCopy = document.querySelector('#generate_btn_copy');
+const generateInput = document.querySelector('.input_generate'),
+      passLength = document.querySelector('.input_length'),
+      inputShowLength = document.querySelector('.length_number'),
+      checkboxUppercase = document.querySelector('.input_uppercase'),
+      checkboxLowercase = document.querySelector('.input_lowercase'),
+      checkboxNumbers = document.querySelector('.input_numbers'),
+      checkboxSymbols = document.querySelector('.input_symbols'),
+      generateBtn = document.querySelector('.generate_btn'),
+      generateBtnCopy = document.querySelector('.btn_copy'),
+      formGenerate = document.querySelector('.generate_container'),
+      copied = document.querySelector('.copied');
 
 function getRandomUpper() {
     const lettersUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -45,8 +47,6 @@ const randomChar = {
     number: getRandomNumbers,
     symbol: getRandomSymbols,
 }
-
-
 
 function generatePassword(upper, lower, number, symbol, length) {
     let gPass = ""
@@ -84,14 +84,54 @@ function innerPass() {
 };
 
 function inputRangeShow() {
-    generateText.textContent = passLength.value;
+    inputShowLength.value = passLength.value;
 };
+
+function InputLengthShow () {
+    passLength.value = inputShowLength.value;
+}
+
+function validateInputs() {
+    if (inputShowLength.value > 40 ) {
+        passLength.value = 40;
+        inputShowLength.value = 40;
+        innerPass();
+    } else if (inputShowLength.value === 0) {
+        passLength.value = 1;
+        inputShowLength.value = 1;
+        innerPass();
+    }
+}
 
 passLength.addEventListener('input', () => {
     inputRangeShow();
     innerPass();
+    validateInputs()
+});
+
+inputShowLength.addEventListener('input', () => {
+    passLength.value = inputShowLength.value;
+    innerPass();
+    validateInputs();
 });
 
 generateBtn.addEventListener('click', () => {
-    innerPass()
+    innerPass();
+    validateInputs();
+    generateBtn.innerText = "Gere outra senha"
+});
+
+formGenerate.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateInputs();
+})
+
+generateBtnCopy.addEventListener('click', () => {
+    generateInput.select();
+    document.execCommand('copy');
+    copied.classList.add('active');
+    setTimeout(()=>{
+        copied.classList.remove('active');
+    }, 1000)
+
 });
