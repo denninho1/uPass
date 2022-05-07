c0c2bd0d9787a24a707a64ae363bb8c197b19d60
Dennis <dennis.dennis2012@gmail.com>
2022-05-07 13:08:05 -0300

    botão option funcionando

--- js/create_new.js
+++ js/create_new.js
@@ -1,10 +1,17 @@
 const btnOptions = document.querySelector('.btn_option');
 const optionsList = document.querySelector('.options_list');
-const options = document.querySelectorAll('.option');
+const options = document.querySelectorAll('.item');
 const selectedOption = document.querySelector('.selected_option');
 
 btnOptions.addEventListener('click', () => {
     optionsList.classList.toggle('active');
-
 });
+
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        let textOption = option.children[1].textContent;
+        let textBtn = btnOptions.children[0];
+        textBtn.textContent = textOption;
+    })
+})
 
