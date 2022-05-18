93e7a57627b69cb23ebf0ea54fc58cdfe61349f2
Dennis <dennis.dennis2012@gmail.com>
2022-05-18 18:07:50 -0300

    refatoração na criação de pastas e abrir forms

--- js/create_new.js
+++ js/create_new.js
@@ -5,54 +5,29 @@ window.addEventListener("load", () => {
    /* cardsValidate() */
 });
 
-/* 
------------ MOSTRAR OPÇÃO ESCOLHIDA NO BOTAO ------------
-*/
-const btnOptions = document.querySelector(".btn_option");
-const optionsList = document.querySelector(".options_list");
-const options = document.querySelectorAll(".item");
-const selectedOption = document.querySelector(".selected_option");
-const createPassForm = document.querySelector(".create_password");
-const createFolderForm = document.querySelector(".create_folder");
-const formCreate = document.querySelector(".forms_create");
-
-btnOptions.addEventListener("click", () => {
-  optionsList.classList.toggle("active");
-});
-
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    let textOption = option.children[1].textContent;
-    let textBtn = btnOptions.children[0];
-    textBtn.textContent = textOption;
-    optionsList.classList.remove("active");
-    //----------- MOSTRAR CAMPOS DA OPÇÃO SELECIONADA ------------
-    if (option === optionsList.children[0]) {
-      createFolderForm.classList.add("active");
-      createPassForm.classList.remove("active");
-      formCreate.style.height = "auto";
-      createFolderForm.style.height = createFolderForm.scrollHeight + "px";
-      createPassForm.style.height = "0px";
-    } else {
-      createPassForm.classList.add("active");
-      createFolderForm.classList.remove("active");
-      formCreate.style.height = "auto";
-      createPassForm.style.height = createPassForm.scrollHeight + "px";
-      createFolderForm.style.height = "0px";
-    }
-  });
-});
 
 /* 
----------- MOSTRAR E FECHAR JANELA MODAL DE NOVA CRIAÇÃO OU COMPARTILHAMENTO --------------
+---------- MOSTRAR E FECHAR MODAL CREATE NEW --------------
 */
 const btnShowModal = document.querySelector(".btn_show_modal");
 const modalCreate = document.querySelector(".create_new_modal");
 const closeModal = document.querySelectorAll(".close_modal");
+const createPassForm = document.querySelector(".create_password");
+const createFolderForm = document.querySelector(".create_folder");
 
 btnShowModal.addEventListener("click", () => {
   showModal();
 });
+
+function showFormPass() {
+    createPassForm.classList.add('active');
+    createFolderForm.classList.remove('active');
+};
+
+function showFormFolder() {
+  createFolderForm.classList.add('active');
+  createPassForm.classList.remove('active');
+};
 
 function showModal() {
   modalCreate.classList.add("active");
@@ -60,18 +35,50 @@ function showModal() {
   window.document.body.style.overflow = "hidden";
 }
 
-closeModal.forEach((close) => {
+closeModal.forEach(close => {
   close.addEventListener("click", () => {
     let textBtn = btnOptions.children[0];
     textBtn.textContent = "Selecione uma opção";
-    modalCreate.classList.remove("active");
     optionsList.classList.remove("active");
-    createFolderForm.classList.remove("active");
-    createPassForm.classList.remove("active");
     btnShowModal.style.display = "block";
-    formCreate.style.height = "250px";
+    modalCreate.style.height = "250px";
+    modalCreate.classList.remove('active')
     window.document.body.style.overflow = "auto";
+    showForms();
   });
+});
+
+/* 
+----------- MOSTRAR OPÇÃO ESCOLHIDA NO BOTAO ------------
+*/
+const btnOptions = document.querySelector(".btn_options");
+const optionsList = document.querySelector(".options_list");
+const options = document.querySelectorAll(".option");
+
+btnOptions.addEventListener("click", () => {
+  optionsList.classList.toggle("active");
+});
+
+options.forEach(option => {
+  option.addEventListener("click", () => {    
+    let textOption = option.children[1].textContent;
+    let textBtn = btnOptions.children[0];
+    textBtn.textContent = textOption;
+    optionsList.classList.remove("active");
+
+    //----------- MOSTRAR CAMPOS DA OPÇÃO SELECIONADA ------------
+    if (option.id === 'folder') {
+      showFormFolder()
+      modalCreate.style.height = "auto";
+      createFolderForm.style.height = createFolderForm.scrollHeight + "px";
+      createPassForm.style.height = "0px";
+    } else {
+      showFormPass();
+      modalCreate.style.height = "auto";
+      createPassForm.style.height = createPassForm.scrollHeight + "px";
+      createFolderForm.style.height = "0px";
+    }
+  }); 
 });
 
 /* 
@@ -98,27 +105,54 @@ function cardsValidate() {
 } */
 
 /* 
------------- CREATE FOLDER ---------
+------------ CREATE NEW FOLDER ---------
 */
 const inputNameFolder = document.querySelector(".new_folder");
 const newFolderBtn = document.querySelector(".btn_create_folder");
 
 function createFolder() {
+  // Nome digitado no input para criar pasta
   let NewFolderName = inputNameFolder.value;
+
+  // Criação do elemento li para lista de pasta
+  const folderContainer = document.createElement('li');
+  folderContainer.classList.add('card')
+
+  // Elemento img para imagem da pasta padrão
+  const folderImg = document.createElement('img');
+  folderImg.src = "../images/pasta icon.svg";
+  folderImg.classList.add('img_folder')
+
+  // Elemento h2 contendo o nome da pasta
+  const folderName = document.createElement('h2');
+  folderName.classList.add('folder_name');
+  folderName.innerHTML = NewFolderName;
+
+  // Elemento span para mostrar total de senhas salvas dentro daquela pasta
+  const folderTotalPass = document.createElement('span');
+  folderTotalPass.classList.add('total_pass')
+  let totalPass = '0'
+  folderTotalPass.innerHTML = `${totalPass} senhas`
+
+  // Elemento a para redirecionamento da senhas
+  const folderLink = document.createElement('a')
+  folderLink.href = '#'
+  folderLink.classList.add('folder_link')
+
+  folderContainer.appendChild(folderImg)
+  folderContainer.appendChild(folderName)
+  folderContainer.appendChild(folderTotalPass)
+  folderContainer.appendChild(folderLink)
 
   if (NewFolderName === "") {
     alert("Preencha o campo");
   } else if (NewFolderName.length < 2) {
     alert('Minimo 2 caracteres')
   } else {
-    if (cards.children.length <= 2) {
-      let newFolder =
-      '<li class="card"><div class="imgs_wrapper"><img src="../images/netflix.png" alt=""></div><div class="text"><h2 id="name_folder">' 
-      +NewFolderName+
-      '</h2><span id="total_pass">0 senhas</span></div><div class="config_card"><i class="fas fa-ellipsis-h"></i></div><a href="#"></a></li>';
-      cards.innerHTML += newFolder;
+    if (cardsContainer.length === 2) {
+      alert('Você já o atingiu o limite de pastas (3)')
     } else {
-        alert('Você já o atingiu o limite de pastas (3)')
+      cardsContainer.appendChild(folderContainer);
     }
   }
 }
@@ -132,6 +166,10 @@ inputNameFolder.addEventListener('input', () => {
         inputNameFolder.setAttribute('maxLength', '15')
     }
 });
+
+
+
+
 
 /* 
 -------- FORMS PREVENT DEFAULT --------
